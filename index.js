97c8dd8ea322c9afd5f20a032dcc8202d299853f
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2022-04-08 10:48:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURICom
 
 Description: ${metadata.desc}
 
-Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`} )
+🇼 🇪 🇱 🇨 🇴 🇲 🇪 🤗!`} )
                 } else if (anu.action == 'remove') {
                     XeonBotInc.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} Left ${metadata.subject}
 
